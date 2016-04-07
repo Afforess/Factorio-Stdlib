7e252e011fb60359d6cd46cce0f742808adbaa39
Afforess <afforess@gmail.com>
2016-04-06 22:10:01 -0400

    Add inventory.copy_inventory

---
+++ spec/entity/inventory_spec.lua
@@ -0,0 +1,26 @@
+require 'stdlib/entity/inventory'
+
+describe('Game Spec', function()
+    it('should copy the inventory', function()
+        function make_get_contents(inv)
+            inv.get_contents = function()
+                local contents = {}
+                for _, item in ipairs(inv.contents) do
+                    contents[item.name] = item.count
+                end
+                return contents
+            end
+        end
+
+        local src_inventory = { contents = { { name = "foo", count = 100}, { name = "bar", count = 33 }, { name = "bar", count = 67 } } }
+        make_get_contents(src_inventory)
+
+        local dest_inventory = { contents = { } }
+        make_get_contents(dest_inventory)
+        dest_inventory.insert = spy(function(stack) table.insert(dest_inventory.contents, stack); return stack.count end)
+
+        assert.same(0, #Inventory.copy_inventory(src_inventory, dest_inventory))
+        assert.spy(dest_inventory.insert).was_called()
+        assert.same(src_inventory.get_contents(), dest_inventory.get_contents())
+    end)
+end)
---
+++ stdlib/entity/inventory.lua
@@ -0,0 +1,21 @@
+--- Inventory module
+-- @module inventory
+
+Inventory = {}
+
+--- Copies an inventory contents to a destination inventory
+-- @param src source inventory to copy from
+-- @param dest destination inventory, to copy to
+-- @return an array of SimpleItemStacks of left over items that could not be copied.
+function Inventory.copy_inventory(src, dest)
+    local contents = src.get_contents()
+    local left_over = {}
+    for n, c in pairs(contents) do
+        local inserted = dest.insert({name=n, count=c})
+        local amt_not_inserted = c - inserted
+        if amt_not_inserted > 0 then
+            table.insert(left_over, { name = n, count = amt_not_inserted })
+        end
+    end
+    return left_over
+end

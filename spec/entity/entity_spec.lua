ac40d49bd61d89bb67af7063996caec60aeca65a
Afforess <afforess@gmail.com>
2016-04-23 14:47:46 -0400

    Add Entity.has for safe field access

--- spec/entity/entity_spec.lua
+++ spec/entity/entity_spec.lua
@@ -14,4 +14,14 @@ describe('Entity Spec', function()
         assert.same({ x = 1, y = -3.5 }, Entity.to_collision_area(entity).left_top)
         assert.same({ x = 3, y = -1.5 }, Entity.to_collision_area(entity).right_bottom)
     end)
+
+    it('should verify Entity.has identifies fields the entity can read from', function()
+        local entity = { backer_name = 'foo' }
+        -- create a metatable to error on key 'health', but allow all other field access
+        local metatbl = { __index = function(tbl, key) if key == 'health' then error('') else return rawget(tbl, key) end end }
+        setmetatable(entity, metatbl)
+
+        assert.truthy(Entity.has(entity, 'backer_name'))
+        assert.falsy(Entity.has(entity, 'health'))
+    end)
 end)

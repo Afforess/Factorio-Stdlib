c425649d052ea347dd5aa4e30545a4e7280ad6d7
Choumiko <ghostface@ghostface.info>
2016-05-07 02:06:18 +0200

    changed string.starts/ends_with to actually return true/false

--- stdlib/string.lua
+++ stdlib/string.lua
@@ -6,14 +6,12 @@ function string.trim(s)
   return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
---Shamelessly stolen from http://lua-users.org/wiki/StringRecipes
-
 --- Tests if a string starts with a given substring
 -- @param s the string to check for the start substring
 -- @param start the substring to test for
 -- @return true if the start substring was found in the string
 function string.starts_with(s, start)
-   return string.sub(s, 1, string.len(start)) == start
+   return string.find(s, start, 1, true) == 1
 end
 
 --- Tests if a string ends with a given substring
@@ -21,5 +19,5 @@ end
 -- @param ends the substring to test for
 -- @return true if the end substring was found in the string
 function string.ends_with(s, ends)
-   return ends == '' or string.sub(s, -string.len(ends)) == ends
+   return #s >= #ends and string.find(s, ends, #s - #ends + 1, true) and true or false
 end

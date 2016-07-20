6fe2d39f4d8a2c4c855e318b28ac5a025306b45f
Credomane <github@credomane.com>
2016-07-20 00:34:00 -0500

    Update tests for Gui.Event
    Some are still failing

--- spec/gui/gui_spec.lua
+++ spec/gui/gui_spec.lua
@@ -1,6 +1,11 @@
 require 'stdlib/gui/gui'
 require 'spec/defines'
 
+--[[in factorio 0.13
+defines.events.on_gui_click=1
+defines.events.on_text_changed=2
+defines.events.on_checkbox_state_changed=3
+--]]
 describe('Event', function()
     before_each(function()
         _G.game = {tick = 1}
@@ -8,15 +13,106 @@ describe('Event', function()
     end)
 
     after_each(function()
-        Event._registry = {}
+        Gui.Event._registry = {}
     end)
 
-    it('.on_click should add multiple callbacks for the same element', function()
-        Gui.on_click("test_element", function() end)
-        Gui.on_click("test_element", function() end)
 
-        assert.truthy(Event._registry[defines.events.on_gui_click][1])
-        assert.truthy(Event._registry[defines.events.on_gui_click][2])
+    it('.register should fail if a nil/false event id is passed', function()
+        assert.has.errors(function() Gui.Event.register( false, "pattern1", function_a ) end)
+        assert.has.errors(function() Gui.Event.register( nil, "pattern1", function_a ) end)
+    end)
+
+    it('.register should fail if a nil/false event id is passed', function()
+        assert.has.errors(function() Gui.Event.register( 1, false, function_a ) end)
+        assert.has.errors(function() Gui.Event.register( 1, nil, function_a ) end)
+    end)
+
+    it('.register should remove handler if nil is passed as a handler', function()
+        Gui.Event.register( 1, "pattern1", function_a )
+        Gui.Event.register( 1, "pattern1", nil)
+
+        assert.is_nil( Gui.Event._registry[1] )
+    end)
+
+    it('.register should keep other patterns if different one is removed', function()
+        Gui.Event.register( 1, "pattern1", function_a )
+        Gui.Event.register( 1, "pattern2", nil)
+
+        assert.is_nil( Gui.Event._registry[1] )
+        assert.is_not_nil( Gui.Event._registry[1]["pattern1"] )
+    end)
+
+    it('.register should hook the event to script.on_event', function()
+        local s = spy.on(script, "on_event")
+        Gui.Event.register( 1, "pattern1", function_a )
+        assert.spy(s).was_called_with(1, Gui.Event.dispatch)
+    end)
+
+    it('.register should return itself', function()
+        assert.equals( Gui.Event, Gui.Event.register( 1, "pattern1", function_a ) )
+        assert.equals( Gui.Event, Gui.Event.register( 1, "pattern2", function_b ).register( 0, "pattern3", function_c ) )
+
+        assert.equals( function_a, Gui.Event._registry[1]["pattern1"] )
+        assert.equals( function_b, Gui.Event._registry[1]["pattern2"] )
+        assert.equals( function_c, Gui.Event._registry[1]["pattern3"] )
+    end)
+
+    it('.dispatch should cascade through registered handlers', function()
+        Gui.Event.register( 1, "pattern1", function_a )
+        Gui.Event.register( 1, "pattern2", function_b )
+        local event = {name = 1, tick = 9001, element={name="pattern1"}, player_index = 1}
+        local s = spy.on(test_function, "f")
+        Gui.Event.dispatch(event)
+        assert.spy(s).was_called_with(9001)
+        assert.spy(s).was_called_with(1)
+        assert.equals(1, someVariable)
+    end)
+
+    it('.dispatch should abort if a handler returns true', function()
+        Gui.Event.register( 1, "pattern1", function_a )
+        Gui.Event.register( 1, "pattern2", function_c )
+        Gui.Event.register( 1, "pattern3", function_b )
+        local event = {name = 1, tick = 9001, element={name="pattern1"}, player_index = 1}
+        local s = spy.on(test_function, "f")
+        Gui.Event.dispatch(event)
+        assert.spy(s).was_called_with(9001)
+        assert.spy(s).was_not_called_with(1)
+        assert.equals(9001, someVariable)
+    end)
+
+    it('.remove should remove the given handler from the event for pattern', function()
+        Gui.Event.register( 1, "pattern1", function_a )
+        Gui.Event.register( 1, "pattern2", function_c )
+        Gui.Event.register( 1, "pattern3", function_b )
+        Gui.Event.register( 1, "pattern4", function_c )
+
+        Gui.Event.remove( 1, "pattern4" )
+
+        assert.is_true( #Gui.Event._registry[1] == 2)
+        assert.equals( function_a, Gui.Event._registry[1]["pattern1"] )
+        assert.equals( function_b, Gui.Event._registry[1]["pattern2"] )
+    end)
+
+    it('.dispatch should print an error to connected players if a handler throws an error', function()
+        _G.game.players = { { name = 'test_player', valid = true, connected = true, print = function(msg) end } }
+        local s = spy.on(_G.game.players[1], "print")
+
+        Gui.Event.register( 1, "pattern1", function() error("should error") end)
+        Gui.Event.dispatch({name = 1, tick = 9001, element={name="pattern1"}, player_index = 1})
+        assert.spy(s).was_called()
+    end)
+
+    it('.dispatch should error when there are no connected players if a handler throws an error', function()
+        _G.game.players = { { name = 'test_player', valid = true, connected = false, print = function(msg) end } }
+        local s = spy.on(_G.game.players[1], "print")
+
+        Gui.Event.register( 0, "pattern1", function() error("should error") end)
+
+        -- verify error was raised
+        local success, err = pcall(Gui.Event.dispatch, {name = 0, tick = 9001, element={name="pattern1"}, player_index = 1})
+        assert.spy(s).was_not_called()
+        assert.is_false(success)
+        assert.is_true(err:contains("should error"))
     end)
 
     it('.on_click should fail if a nil/false gui element pattern is passed', function()
@@ -27,19 +123,5 @@ describe('Event', function()
     it('.on_click should return itself', function()
         assert.equals(Gui, Gui.on_click("test_element", function() end))
         assert.equals(Gui, Gui.on_click("test_element", function() end).on_click("test_element", function() end))
-    end)
-
-    it('.on_click should cascade through registered handlers', function()
-        local test_function = { f=function(bar) _G.foo = bar end }
-        local func_a = function(arg) test_function.f(arg.tick) end
-        local func_b = function(arg) test_function.f(arg.player_index) end
-        Gui.on_click("test_element", func_a)
-        Gui.on_click("test_element", func_b)
-        local event = {name = defines.events.on_gui_click, tick = 9001, element = { name = "test_element_foobar", valid = true }, player_index = 1}
-        local s = spy.on(test_function, "f")
-        Event.dispatch(event)
-        assert.spy(s).was_called_with(9001)
-        assert.spy(s).was_called_with(1)
-        assert.equals(1, _G.foo)
     end)
 end)
--- stdlib/gui/gui.lua
+++ stdlib/gui/gui.lua
@@ -53,7 +53,10 @@ function Gui.Event.dispatch(event)
                 if event.name == defines.events.on_gui_text_changed then new_event.text = gui_element.text end
                 local success, err = pcall(handler, new_event)
                 if not success then
-                    Game.print_all(err)
+                    if Game.print_all(err) == 0 then
+                        -- no players received the message, force a real error so someone notices
+                        error(err)
+                    end
                 end
             end
         end

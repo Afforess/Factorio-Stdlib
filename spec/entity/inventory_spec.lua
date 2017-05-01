2883142428ba3771901e78b790714763fa7a4a7c
Nexela <nexela@gmail.com>
2017-05-01 07:57:19 -0400

    Fix up Inventory unit checks, Rename function

--- spec/entity/inventory_spec.lua
+++ spec/entity/inventory_spec.lua
@@ -1,4 +1,5 @@
 require 'stdlib/entity/inventory'
+require('stdlib.table')
 
 describe('Game Spec', function()
     function make_get_contents(inv)
@@ -20,22 +21,31 @@ describe('Game Spec', function()
     end
 
     it('should copy an inventory', function()
-        local src_inventory = { { name = "foo", count = 100, valid = true, valid_for_read = true, prototype = {} }, { name = "bar", count = 33, durability = 84, valid = true, valid_for_read = true, prototype = {}  }, { name = "bar", count = 67, valid = true, valid_for_read = true, prototype = {} } }
+        local src_inventory = {
+            { name = "foo", count = 100, valid = true, valid_for_read = true, prototype = {} },
+            { name = "bar", count = 33, durability = 84, valid = true, valid_for_read = true, prototype = {}  },
+            { name = "bar", count = 67, valid = true, valid_for_read = true, prototype = {} } }
         make_get_contents(src_inventory)
         local dest_inventory = {}
         make_get_contents(dest_inventory)
-        dest_inventory.insert = spy(function(stack) table.insert(dest_inventory, stack); return stack.count end)
+        dest_inventory.insert = spy(function(stack)
+            setmetatable(stack, {__index = {valid = true}})
+            table.insert(dest_inventory, table.deepcopy(stack))
+            return stack.count end
+         )
 
-        assert.same(0, #Inventory.copy_inventory(src_inventory, dest_inventory))
+        assert.same(0, #Inventory.copy_as_simple_stacks(src_inventory, dest_inventory))
         assert.spy(dest_inventory.insert).was_called()
         assert.same(src_inventory.get_contents()['foo'], dest_inventory.get_contents()['foo'])
         assert.same(src_inventory.get_contents()['bar'], dest_inventory.get_contents()['bar'])
         assert.same(#src_inventory, #dest_inventory)
     end)
 
-
     it('should copy ammo inventory', function()
-        local src_inventory = { { name = "bullets", count = 100, ammo = 345, valid = true, valid_for_read = true, prototype = { ammo_type = {}} }, { name = "foobar", count = 35, valid = true, valid_for_read = true, prototype = {} } }
+        local src_inventory = {
+            { name = "bullets", count = 100, ammo = 345, valid = true, valid_for_read = true, prototype = { ammo_type = {}} },
+            { name = "foobar", count = 35, valid = true, valid_for_read = true, prototype = {} }
+        }
         -- emulate error on accessing 'ammo' field on non-ammo items
         for _, item in pairs(src_inventory) do
             setmetatable(item, {__index = function(tbl, key) if key == 'ammo' then error("Accessing ammo on non-ammo items is not allowed!") else return rawget(tbl, key) end end})
@@ -46,12 +56,75 @@ describe('Game Spec', function()
 
         local dest_inventory = {}
         make_get_contents(dest_inventory)
-        dest_inventory.insert = spy(function(stack) table.insert(dest_inventory, stack); return stack.count end)
+        dest_inventory.insert = spy(function(stack)
+            setmetatable(stack, {__index = {valid = true}})
+            table.insert(dest_inventory, table.deepcopy(stack))
+            return stack.count end
+         )
 
-        assert.same(0, #Inventory.copy_inventory(src_inventory, dest_inventory))
+        assert.same(0, #Inventory.copy_as_simple_stacks(src_inventory, dest_inventory))
         assert.spy(dest_inventory.insert).was_called()
         assert.same(src_inventory.get_contents()['ammo'], dest_inventory.get_contents()['ammo'])
         assert.same(src_inventory.get_contents()['foobar'], dest_inventory.get_contents()['foobar'])
         assert.same(#src_inventory, #dest_inventory)
+    end)
+
+    it('should clear the source inventory', function()
+        local src_inventory = {
+            { name = "foo", count = 100, valid = true, valid_for_read = true, prototype = {} },
+            { name = "bar", count = 33, durability = 84, valid = true, valid_for_read = true, prototype = {}  },
+            { name = "bar", count = 67, valid = true, valid_for_read = true, prototype = {} } }
+        make_get_contents(src_inventory)
+        local dest_inventory = {}
+        make_get_contents(dest_inventory)
+        dest_inventory.insert = spy(function(stack)
+            setmetatable(stack, {__index = {valid = true}})
+            table.insert(dest_inventory, table.deepcopy(stack))
+            return stack.count end
+         )
+        src_inventory.clear = spy(function()
+            for i = #src_inventory, 1, -1 do
+                src_inventory[i] = nil
+            end
+        end)
+
+        assert.same(0, #Inventory.copy_as_simple_stacks(src_inventory, dest_inventory, true))
+        assert.spy(dest_inventory.insert).was_called()
+        assert.spy(src_inventory.clear).was_called()
+        assert.falsy(src_inventory.get_contents()['foo'])
+        assert.same(100, dest_inventory.get_contents()['foo'])
+        assert.falsy(src_inventory.get_contents()['bar'])
+        assert.same(100, dest_inventory.get_contents()['bar'])
+    end)
+
+    it('should return item stacks not inserted', function()
+        local src_inventory = {
+            { name = "foo", count = 100, valid = true, valid_for_read = true, prototype = {} },
+            { name = "bar", count = 33, durability = 84, valid = true, valid_for_read = true, prototype = {}  },
+            { name = "bar", count = 67, valid = true, valid_for_read = true, prototype = {} } }
+        make_get_contents(src_inventory)
+        local dest_inventory = {}
+        make_get_contents(dest_inventory)
+        dest_inventory.insert = spy(function(stack)
+            setmetatable(stack, {__index = {valid = true}})
+            stack.count = stack.name == "bar" and 0 or stack.name =='foo' and stack.count - 20
+            if stack.count > 0 then table.insert(dest_inventory, table.deepcopy(stack)) end
+            return stack.count end
+         )
+        src_inventory.clear = spy(function()
+            for i = #src_inventory, 1, -1 do
+                src_inventory[i] = nil
+            end
+        end)
+        local returned = Inventory.copy_as_simple_stacks(src_inventory, dest_inventory, true)
+        assert.same(3, #returned)
+        assert.spy(dest_inventory.insert).was_called()
+        assert.spy(src_inventory.clear).was_called()
+        assert.same(20, returned[1].count)
+        assert.same(33, returned[2].count)
+        assert.falsy(src_inventory.get_contents()['foo'])
+        assert.same(80, dest_inventory.get_contents()['foo'])
+        assert.falsy(src_inventory.get_contents()['bar'])
+        assert.falsy(dest_inventory.get_contents()['bar'])
     end)
 end)
--- stdlib/entity/inventory.lua
+++ stdlib/entity/inventory.lua
@@ -5,12 +5,12 @@ local fail_if_missing = require 'stdlib/core'['fail_if_missing']
 
 Inventory = {} --luacheck: allow defined top
 
---- Copies an inventory contents to a destination inventory
--- <b>Deprecated</b>, refer to https://github.com/Afforess/Factorio-Stdlib/issues/66 for details.
+--- Copies an inventory contents to a destination inventory using simple item stacks.
 -- @param src source inventory to copy from
 -- @param dest destination inventory, to copy to
--- @return an array of SimpleItemStacks of left over items that could not be copied.
-function Inventory.copy_inventory(src, dest)
+-- @param clear clear the contents of the source inventory
+-- @return an array of SimpleItemStacks of left over items that could not be inserted into dest.
+function Inventory.copy_as_simple_stacks(src, dest, clear)
     fail_if_missing(src, "missing source inventory")
     fail_if_missing(dest, "missing destination inventory")
 
@@ -22,14 +22,10 @@ function Inventory.copy_inventory(src, dest)
                 name = stack.name,
                 count = stack.count,
                 health = stack.health or 1,
-                durability = stack.durability or nil
+                durability = stack.durability
             }
-            --Valid metatable is not needed. SimpleItemStack is and unrefrenced tables.
-
             -- ammo is a special case field, accessing it on non-ammo itemstacks causes an exception
-            if stack.prototype.ammo_type then
-                simple_stack.ammo = stack.ammo or nil
-            end
+            simple_stack.ammo = stack.prototype.ammo_type and stack.ammo
 
             --Insert simple stack into inventory, add to left_over if not all were inserted.
             simple_stack.count = simple_stack.count - dest.insert(simple_stack)
@@ -38,6 +34,7 @@ function Inventory.copy_inventory(src, dest)
             end
         end
     end
+    if clear then src.clear() end
     return left_over
 end
 

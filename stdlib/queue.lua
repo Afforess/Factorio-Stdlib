961e046bed026e83295d28dfbddb8a3904174887
Pandemoneus <Pandemoneus@gmx.de>
2017-06-05 23:13:00 +0200

    Added double queue and resource patch scanning

---
+++ stdlib/queue.lua
@@ -0,0 +1,61 @@
+--- A double queue.
+-- Taken from "Programming in Lua" (http://www.lua.org/pil/11.4.html)
+-- Modified to not allow nil values, and returns nil if pop_left or pop_right is used when the queue is empty.
+local fail_if_missing = require 'stdlib/core'['fail_if_missing']
+
+Queue = {}
+
+function Queue.new ()
+  return {first = 0, last = -1}
+end
+
+function Queue.push_left(queue, value)
+    fail_if_missing(queue)
+    fail_if_missing(value)
+
+    local first = queue.first - 1
+    queue.first = first
+    queue[first] = value
+end
+
+function Queue.push_right(queue, value)
+    fail_if_missing(queue)
+    fail_if_missing(value)
+
+    local last = queue.last + 1
+    queue.last = last
+    queue[last] = value
+end
+   
+function Queue.pop_left(queue)
+    fail_if_missing(queue)
+
+    if Queue.is_empty(queue) then 
+        return nil
+    end
+
+    local first = queue.first
+    local value = queue[first]
+    queue[first] = nil        -- to allow garbage collection
+    queue.first = first + 1
+    return value
+end
+    
+function Queue.pop_right(queue)
+    fail_if_missing(queue)
+
+    if Queue.is_empty(queue) then 
+        return nil
+    end
+
+    local last = queue.last
+    local value = queue[last]
+    queue[last] = nil         -- to allow garbage collection
+    queue.last = last - 1
+    return value
+end
+
+function Queue.is_empty(queue)
+    fail_if_missing(queue)
+
+    return queue.first > queue.last
---
+++ stdlib/resources/resources.lua
@@ -0,0 +1,81 @@
+local Position = require 'stdlib/area/position'
+local Surface = require 'stdlib/area/surface'
+local Tile = require 'stdlib/area/tile'
+local Queue = require 'stdlib/queue'
+
+Resources = {}
+
+--- Get the resource entity at the specified position and surface.
+-- Adapted from YARM/resmon.lua -> find_resource_at
+-- @param surface the surface of the position
+-- @param position the position to check
+-- @return the resource entity or nil if there is no resource there
+function Resources.get_resource_at(surface, position)
+    local surfaces = Surface.lookup(surface)
+
+    if #surfaces == 0 then
+        return nil
+    end
+
+    local tile_at_position = Tile.from_position(position)
+    local tile_area = Tile.to_area(tile_at_position)
+
+    local resource_at_tile = surface.find_entities_filtered{area=tile_area, type='resource'}
+    
+    if #resource_at_tile < 1 then
+        return nil
+    end
+
+    return resource_at_tile[1] -- there should never be another resource at the exact same coordinates
+end
+
+--- From the resource at the given surface and position, return all connected (horizontally, vertically and diagonally) resource entities of the same type.
+-- (For now, just ore patches. Problems arise when a single resource entity spans multiple tiles).
+-- @param surface the surface of the position
+-- @param position the position to check
+-- @return a table containing the resource entity at the given position and all connected resource entities of the same type or an empty table if there is no resource there
+function Resources.get_resource_patch_at(surface, position)
+    -- get the initial resource tile if there is one at the given position
+    local initial_resource_entity = Resources.get_resource_at(surface, position)
+    if not initial_resource_entity then
+        return {}
+    end
+
+    -- then search around it and expand it to the whole resource patch
+    local resource_patch = {initial_resource_entity}
+    local initial_tile = Tile.from_position(initial_resource_entity.position)
+
+    -- keep a table of visited tiles so we do not inspect tiles twice
+    local resource_type = initial_resource_entity.name
+    local visited_tiles = {Tile.get_index(initial_tile)}
+
+    local search_queue = Queue.new()
+    Queue.push_right(search_queue, initial_tile)
+
+    while not Queue.is_empty(search_queue) do
+        local current_tile = Queue.pop_left(search_queue)
+        local current_tile_index = Tile.get_index(current_tile)
+
+        -- we already got this tile, skip it
+        if visited_tiles[current_tile_index] then
+            continue
+        end
+
+        local current_entity = Resources.get_resource_at(surface, current_tile)
+
+        -- no resource at this tile or it is of another resource type, skip this tile
+        if (not current_entity) or (current_entity.name != resource_type) then
+            continue
+        end
+
+        -- this tile belongs to the ore patch, now add all tiles around it to the search queue
+        table.insert(resource_patch, current_entity)
+        table.insert(visited_tiles, current_tile_index)
+
+        for adjacent_tile in Tiles.adjacent(surface=surface, position=current_tile, diagonal=true) do
+            Queue.push_right(search_queue, adjacent_tile)
+        end
+    end
+
+    return resource_patch
+end

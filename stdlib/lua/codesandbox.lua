98a68896d63d11bee637e0a55781513fd00e762d
NicholasScott1337 <daymonforeman14@gmail.com>
2017-06-05 01:39:45 -0500

    Update codesandbox.lua

--- stdlib/lua/codesandbox.lua
+++ stdlib/lua/codesandbox.lua
@@ -6,13 +6,14 @@ Sandbox._ENV = {}
 function Sandbox.SetEnvironmentVariables(self, env)
   self._ENV = env
 end
-function Sandbox.ExecuteCode(self, code, isByteCode) --code: string, isByteCode: bool // If you don't know what this is just don't pass it
+function Sandbox.ExecuteCode(code, isByteCode) --code: string, isByteCode: bool // If you don't know what this is just don't pass it
   local code_Func, message = load(code, nil, isByteCode and "b" or "t", self._ENV)
   if not code_Func then return nil, message end
   local ret, err = xpcall(function()
     debug.sethook(function() error("Script took too long to respond!") end, "", 1e8) -- Too protect against `while true do end` abuse
     local ret = code_Func()
     debug.sethook() --Clear the hook
+    return ret
   end, debug.traceback)
   return ret, err
 end

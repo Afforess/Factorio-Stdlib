2f8a6cbdfe713acf11c7969ee0f086234ecdc8a7
Pandemoneus <Pandemoneus@gmx.de>
2017-06-06 04:05:02 +0200

    Inverted the condition since there is no continue in lua

--- stdlib/entity/resource.lua
+++ stdlib/entity/resource.lua
@@ -1,16 +1,20 @@
+--- Resource utilities.
+-- @module Resource
+-- @usage local Resource = require('stdlib/entity/resource')
+
 local Position = require 'stdlib/area/position'
 local Surface = require 'stdlib/area/surface'
 local Tile = require 'stdlib/area/tile'
 local Queue = require 'stdlib/queue'
 
-Resources = {}
+Resource = {}
 
 --- Get the resource entity at the specified position and surface.
 -- Adapted from YARM/resmon.lua -> find_resource_at
--- @param surface the surface of the position
--- @param position the position to check
--- @return the resource entity or nil if there is no resource there
-function Resources.get_resource_at(surface, position)
+-- @tparam LuaSurface surface the surface of the position
+-- @tparam LuaPosition position the position to check
+-- @treturn LuaEntity|nil the resource entity or nil if there is no resource there
+function Resource.get_resource_at(surface, position)
     local surfaces = Surface.lookup(surface)
 
     if #surfaces == 0 then
@@ -31,12 +35,12 @@ end
 
 --- From the resource at the given surface and position, return all connected (horizontally, vertically and diagonally) resource entities of the same type.
 -- (For now, just ore patches. Problems arise when a single resource entity spans multiple tiles).
--- @param surface the surface of the position
--- @param position the position to check
--- @return a table containing the resource entity at the given position and all connected resource entities of the same type or an empty table if there is no resource there
-function Resources.get_resource_patch_at(surface, position)
+-- @tparam LuaSurface surface the surface of the position
+-- @tparam LuaPosition position the position to check
+-- @treturn {LuaEntity*} a table containing the resource entity at the given position and all connected resource entities of the same type or an empty table if there is no resource there
+function Resource.get_resource_patch_at(surface, position)
     -- get the initial resource tile if there is one at the given position
-    local initial_resource_entity = Resources.get_resource_at(surface, position)
+    local initial_resource_entity = Resource.get_resource_at(surface, position)
     if not initial_resource_entity then
         return {}
     end
@@ -54,26 +58,25 @@ function Resources.get_resource_patch_at(surface, position)
 
     while not Queue.is_empty(search_queue) do
         local current_tile = Queue.pop_left(search_queue)
-        local current_entity = Resources.get_resource_at(surface, current_tile)
+        local current_entity = Resource.get_resource_at(surface, current_tile)
         local current_tile_index = Tile.get_index(current_tile)
 
         table.insert(visited_tiles, current_tile_index)
 
-        -- no resource at this tile or it is of another resource type, skip this tile
-        if (not current_entity) or (current_entity.name != resource_type) then
-            continue
-        end
+        if current_entity and current_entity.name == resource_type then
+            -- this tile belongs to the ore patch
+            table.insert(resource_patch, current_entity)
 
-        -- this tile belongs to the ore patch
-        table.insert(resource_patch, current_entity)
-
-        -- add all tiles around this one that we did not visit yet
-        for adjacent_tile in Tile.adjacent(surface=surface, position=current_tile, diagonal=true) do
-            if not visited_tiles[Tile.get_index(adjacent_tile)] then
-                Queue.push_right(search_queue, adjacent_tile)
+            -- add all tiles around this one that we did not visit yet
+            for adjacent_tile in Tile.adjacent(surface=surface, position=current_tile, diagonal=true) do
+                if not visited_tiles[Tile.get_index(adjacent_tile)] then
+                    Queue.push_right(search_queue, adjacent_tile)
+                end
             end
         end
     end
 
     return resource_patch
 end
+
+return Resource

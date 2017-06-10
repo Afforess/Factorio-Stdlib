75e729cc8c53073a66163b2b35363f2d6a4705f4
Nexela <nexela@gmail.com>
2017-06-10 04:52:08 -0400

    Don't forget defines

--- stdlib/color/color.lua
+++ stdlib/color/color.lua
@@ -2,6 +2,7 @@
 -- @module Color
 -- @usage local Color = require('stdlib/color/color')
 
+require 'stdlib/defines/color'
 local fail_if_missing = require 'stdlib/core'['fail_if_missing']
 
 Color = {} --luacheck: allow defined top

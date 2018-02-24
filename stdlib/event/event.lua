86ed8bba2bb943f21fa15a33433dca8ba5d5711c
Nicholas Dunn <nexela@gmail.com>
2018-02-23 19:17:06 -0500

    More event Wip

--- stdlib/event/event.lua
+++ stdlib/event/event.lua
@@ -65,7 +65,7 @@ end
 -- @param event_id (<span class="types">@{defines.events}, @{int}, @{string}, or {@{defines.events}, @{int}, @{string},...}</span>)
 -- @tparam function handler the function to call when the given events are triggered
 -- @return (<span class="types">@{Event}</span>) Event module object allowing for call chaining
-function Event.register(event_id, handler, callback)
+function Event.register(event_id, handler, matcher, pattern)
     fail_if_not(event_id, 'missing event_id argument')
     fail_if_not(handler, 'handler is missing, use Event.remove to un register events')
 
@@ -105,7 +105,7 @@ function Event.register(event_id, handler, callback)
     --If handler is already registered for this event: remove it for re-insertion at the end.
     if #registry > 0 then
         for i, registered in ipairs(registry) do
-            if handler == registered.handler then
+            if registered.handler == handler and registered.pattern == pattern and registered.matcher == matcher then
                 table.remove(registry, i)
                 log('Same handler already registered for event ' .. event_id .. ' at position ' .. i .. ', moving it to the bottom')
                 break
@@ -114,7 +114,67 @@ function Event.register(event_id, handler, callback)
     end
 
     --Finally insert the handler
-    table.insert(registry, {handler = handler, callback = callback})
+    table.insert(registry, {handler = handler, pattern = pattern, matcher = matcher})
+    return Event
+end
+
+--- Removes a handler from the given events.
+-- <p>When the last handler for an event is removed, stop listening to that event.
+-- <p>An *event ID* can be obtained via @{defines.events},
+-- @{LuaBootstrap.generate_event_name|script.generate_event_name} which is in <span class="types">@{int}</span>,
+-- and can be a custom input name which is in <span class="types">@{string}</span>.
+-- <p>The `event_id` parameter takes in either a single, multiple, or mixture of @{defines.events}, @{int}, and @{string}.
+-- @param event_id (<span class="types">@{defines.events}, @{int}, @{string}, or {@{defines.events}, @{int}, @{string},...}</span>)
+-- @tparam[opt] function handler the handler to remove, if not present remove all registered handlers for the event_id
+-- @return (<span class="types">@{Event}</span>) Event module object allowing for call chaining
+function Event.remove(event_id, handler, matcher, pattern)
+    fail_if_not(event_id, 'missing event_id argument')
+
+    -- Handle recursion here
+    if Is.Table(event_id) then
+        for _, id in pairs(event_id) do
+            Event.remove(id, handler)
+        end
+        return Event
+    end
+
+    fail_if_not(valid_id(event_id))
+
+    local registry = event_registry[event_id]
+    if registry then
+        for i = #registry, 1, -1 do
+            local registered = registry[i]
+
+            -- TODO better removing if something is not present
+            if not handler and not pattern and not matcher then
+                table.remove(registered, i)
+                break
+            elseif handler == registered.handler and pattern == registered.pattern and matcher == registered.matcher then
+                table.remove(registered, i)
+                break
+            end
+        end
+
+        -- Clear the registry data and un subscribe if there are no registered handlers left
+        if table.size(registry) == 0 then
+            event_registry[event_id] = nil
+
+            if Is.String(event_id) then
+                -- String event ids will either be Bootstrap events or custom input events
+                if bootstrap_register[event_id] then
+                    script[event_id](nil)
+                else
+                    script.on_event(event_id, nil)
+                end
+            elseif event_id >= 0 then
+                -- Positive values will be defines.events
+                script.on_event(event_id, nil)
+            elseif event_id < 0 then
+                -- Use negative values to remove on_nth_tick
+                script.on_nth_tick(event_id, nil)
+            end
+        end
+    end
     return Event
 end
 
@@ -163,12 +223,19 @@ function Event.dispatch(event)
                 end
 
                 -- Can't directly put our handler into the event, but we can index into it.
-                setmetatable(event, {__index = {_handler = registered.handler, _callback = registered.callback}})
+                local mt = {
+                    __index = {
+                        _handler = registered.handler,
+                        _matcher = registered.matcher,
+                        _pattern = registered.pattern
+                    }
+                }
+                setmetatable(event, mt)
 
-                -- Call the callback and the handler in protected mode.
+                -- Call the matcher and the handler in protected mode.
                 local success, err
-                if registered.callback then
-                    success, err = pcall(registered.callback, event)
+                if registered.matcher then
+                    success, err = pcall(registered.matcher, event)
                     if success and err then
                         success, err = pcall(registered.handler, event)
                     end
@@ -208,79 +275,14 @@ function Event.dispatch(event)
     end
 end
 
---- Removes a handler from the given events.
--- <p>When the last handler for an event is removed, stop listening to that event.
--- <p>An *event ID* can be obtained via @{defines.events},
--- @{LuaBootstrap.generate_event_name|script.generate_event_name} which is in <span class="types">@{int}</span>,
--- and can be a custom input name which is in <span class="types">@{string}</span>.
--- <p>The `event_id` parameter takes in either a single, multiple, or mixture of @{defines.events}, @{int}, and @{string}.
--- @param event_id (<span class="types">@{defines.events}, @{int}, @{string}, or {@{defines.events}, @{int}, @{string},...}</span>)
--- @tparam[opt] function handler the handler to remove, if not present remove all registered handlers for the event_id
--- @return (<span class="types">@{Event}</span>) Event module object allowing for call chaining
-function Event.remove(event_id, handler, callback_result)
-    fail_if_not(event_id, 'missing event_id argument')
-    fail_if_not(Is.Nil(handler) or Is.Function(handler), 'handler must be nil or a function')
-
-    -- Handle recursion here
-    if Is.Table(event_id) then
-        for _, id in pairs(event_id) do
-            Event.remove(id, handler)
-        end
-        return Event
-    end
-
-    fail_if_not(valid_id(event_id))
-
-    local registry = event_registry[event_id]
-    if registry then
-        for i = #registry, 1, -1 do
-            local registered = registry[i]
-            if not handler and not callback_result then
-                table.remove(registry, i)
-            elseif registered.callback then
-                if registered.callback() == callback_result and registered.handler == handler then
-                    table.remove(registry, i)
-                end
-            elseif registered.handler == handler then
-                table.remove(registry, i)
-            elseif not handler and callback_result and registered.callback then
-                if registered.callback() == callback_result then
-                    table.remove(registry, i)
-                end
-            end
-        end
-
-        -- Clear the registry data and un subscribe if there are no registered handlers left
-        if table.size(registry) == 0 then
-            event_registry[event_id] = nil
-
-            if Is.String(event_id) then
-                -- String event ids will either be Bootstrap events or custom input events
-                if bootstrap_register[event_id] then
-                    script[event_id](nil)
-                else
-                    script.on_event(event_id, nil)
-                end
-            elseif event_id >= 0 then
-                -- Positive values will be defines.events
-                script.on_event(event_id, nil)
-            elseif event_id < 0 then
-                -- Use negative values to remove on_nth_tick
-                script.on_nth_tick(event_id, nil)
-            end
-        end
-    end
-    return Event
-end
-
 --- Filters events related to entity_type.
 -- @tparam string event_parameter The event parameter to look inside to find the entity type
 -- @tparam string entity_type The entity type to filter events for
--- @tparam callable callback The callback to invoke if the filter passes. The object defined in the event parameter is passed
-function Event.filter_entity(event_parameter, entity_type, callback)
+-- @tparam callable matcher The matcher to invoke if the filter passes. The object defined in the event parameter is passed
+function Event.filter_entity(event_parameter, entity_type, matcher)
     return function(evt)
         if (evt[event_parameter].type == entity_type) then
-            callback(evt[event_parameter])
+            matcher(evt[event_parameter])
         end
     end
 end

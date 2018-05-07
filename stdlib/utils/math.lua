b24ee9c410cf7f3bd2278b862f6734ebb59cb1ec
Nicholas Dunn <nexela@gmail.com>
2018-05-07 02:40:57 -0400

    More Math

--- stdlib/utils/math.lua
+++ stdlib/utils/math.lua
@@ -4,10 +4,11 @@
 
 local Math = {}
 
-local floor = math.floor
-local ceil = math.ceil
-local min = math.min
-local max = math.max
+local math_abs = math.abs
+local math_floor = math.floor
+local math_ceil = math.ceil
+local math_min = math.min
+local math_max = math.max
 local log10 = math.log10
 local unpack = table.unpack
 
@@ -15,27 +16,50 @@ for k, v in pairs(math) do
     Math[k] = v
 end
 
+--(( Math Constants
 Math.maxint8 = 127
-Math.minint8 =  -128
+Math.minint8 = -128
 Math.maxuint8 = 255
+
+Math.max_int8 = Math.maxint8
+Math.min_int8 = Math.minint8
+Math.max_uint8 = Math.maxuint8
 
 Math.maxint16 = 32767
 Math.minint16 = -32768
 Math.maxuint16 = 65535
+Math.max_int16 = Math.maxint16
+Math.min_int16 = Math.minint16
+Math.max_uint16 = Math.maxuint16
 
-Math.maxint32 = 2147483647
-Math.minint32 = -2147483648
-Math.maxuint32 = 4294967295
+Math.maxint =  2147483647
+Math.max_int = Math.maxint
+Math.maxint32 = Math.maxint
+Math.max_int32 = Math.maxint
+
+Math.maxuint = 4294967295
+Math.max_uint = Math.maxuint
+Math.maxuint32 = Math.maxuint
+Math.max_uint32 = Math.maxuint
+
+Math.minint = -2147483648
+Math.min_int = Math.minint
+Math.minint32 = Math.minint
+Math.min_int32 = Math.minint
 
 Math.maxint64 = 9223372036854775807
 Math.minint64 = -9223372036854775808
 Math.maxuint64 = 18446744073709551615
+Math.max_int64 = Math.maxint64
+Math.min_int64 = Math.minint64
+Math.max_uint64 = Math.maxuint64
+--))
 
 --- Round a number.
 -- @tparam number x
 -- @treturn number the rounded number
 function Math.round(x)
-    return x >= 0 and floor(x + 0.5) or ceil(x - 0.5)
+    return x >= 0 and math_floor(x + 0.5) or math_ceil(x - 0.5)
 end
 
 -- Various average (means) algorithms implementation
@@ -122,7 +146,7 @@ end
 -- @tparam array x an array of numbers
 -- @treturn number the midrange mean
 function Math.midrange_mean(x)
-    return 0.5 * (min(unpack(x)) + max(unpack(x)))
+    return 0.5 * (math_min(unpack(x)) + math_max(unpack(x)))
 end
 
 --- Calculates the energetic mean of a set of values.
@@ -142,19 +166,42 @@ end
 -- @treturn number floored to p decimal spaces.
 function Math.floor_to(x, p)
     if (p or 0) == 0 then
-        return floor(x)
+        return math_floor(x)
     end
     local e = 10 ^ p
-    return floor(x * e) / e
+    return math_floor(x * e) / e
 end
 
--- Returns the number rounded to p decimal places.
+-- Returns the number x rounded to p decimal places.
 -- @tparam number x
 -- @tparam[opt=0] int p the number of decimal places to round to
 -- @treturn number rounded to p decimal spaces.
 function Math.round_to(x, p)
     local e = 10 ^ (p or 0)
-    return floor(x * e + 0.5) / e
+    return math_floor(x * e + 0.5) / e
+end
+
+--- Returns the number x clamped between the numbers min and max
+-- @tparam number x
+-- @tparam number min
+-- @tparam number max
+-- @treturn number clamped between min and max
+function Math.clamp(x, min, max)
+    return x < min and min or (x > max and max or x)
+end
+
+function Math.lerp(a, b, amount)
+    return a + (b - a) * Math.clamp(amount, 0, 1)
+end
+
+function Math.smooth(a, b, amount)
+    local t = Math.clamp(amount, 0, 1)
+    local m = t * t * (3 - 2 * t)
+    return a + (b - a) * m
+end
+
+function Math.pingpong(x)
+    return 1 - math_abs(1 - x % 2)
 end
 
 -- Overwrite the global table 'math' if the flag is not set.

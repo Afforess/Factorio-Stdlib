7b4b9085ec2f67f752516c6d7719acd2229c6416
Nicholas Dunn <nexela@gmail.com>
2018-08-26 21:56:26 -0400

    Folder Structure Changes

--- data/modules/tester.lua
+++
@@ -1,6 +0,0 @@
---(( TESTS ))--
-local Recipe = require('__stdlib__/data/modules/recipe')
-require('__stdlib__/spec/setup/dataloader')
-_G.log = function(m) print(m) end
-local _ = Recipe('electronic-circuit'):Results():Ingredients():remove('iron-plate', false)():log():Ingredients()
-print(_:has('copper-cable', 'iron-plate'))

ecd8dfdfa14dcfb57467fbcdf1376d12f224eedc
Nicholas Dunn <nexela@gmail.com>
2018-09-12 00:36:19 -0400

    Really make utils global

--- stdlib/utils/globals.lua
+++ stdlib/utils/globals.lua
@@ -10,6 +10,10 @@ traceback = type(debug) == 'table' and debug.traceback or _traceback
 serpent = serpent or require('__stdlib__/stdlib/vendor/serpent')
 inspect = inspect or require('__stdlib__/stdlib/vendor/inspect')
 
+require('__stdlib__/stdlib/utils/math')
+require('__stdlib__/stdlib/utils/table')
+require('__stdlib__/stdlib/utils/string')
+
 -- Set up default stuff for testing, defines will already be available in an active mod or busted setup specs
 if not _G.defines then
     require('__stdlib__/spec/setup/dataloader')

733a4941ecfa45ccb03590000a3ecc7af5e150ed
Nexela <nexela@gmail.com>
2020-01-26 12:55:33 -0500

    Fix Changelog

--- data.lua
+++
@@ -1,5 +0,0 @@
--- Empty file to trick Factorio into thinking this mod changes the game.
--- Without this file STDLIB will not be synced in MP as Factorio thinks
--- it is a non game changing mod (I.E translation only)
-
--- if mods["debugadapter"] then require('__debugadapter__/debugadapter.lua') end

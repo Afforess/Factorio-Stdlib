21a930300aeec2af5f882f0a62016379e1b6bbb0
Nexela <nexela@gmail.com>
2020-02-05 00:11:31 -0500

    StringArray phase 1

--- spec/utils/classes/string_array_spec.lua
+++ spec/utils/classes/string_array_spec.lua
@@ -3,72 +3,123 @@ require('busted.runner')()
 local String_Array = require('__stdlib__/stdlib/utils/classes/string_array')
 
 describe('String_array', function()
+    local base, same
+    before_each(function()
+        base = String_Array{'a', 'b', 'c', 'd'}
+        same = {'a', 'b', 'c', 'd'}
+    end)
 
     it('should attach itself to a table', function()
-        local tab = String_Array({})
+        local tab = String_Array{}
         assert(getmetatable(tab))
         setmetatable(tab, nil)
         String_Array(tab)
         assert(getmetatable(tab))
     end)
 
-    local base = String_Array({'abc', 'def', 'ghi', 'jkl'})
+    --local base = String_Array{'abc', 'def', 'ghi', 'jkl'}
 
-    it('should be able to add unique items', function()
-        base:add('mno')
+    it(':all should return true if it has passed items', function()
+        assert.is_true(base:all('a'))
+        assert.is_true(base:all('a', 'd'))
+        assert.is_true(base:all({'a', 'd'}))
+
+        assert.is_false(base:all('m'))
+        assert.is_false(base:all({'d', 'm'}))
+        assert.is_false(base:all('a', 'm'))
+
+    end)
+
+    it(':any should return true if it has any passed items', function()
+        assert.is_true(base:any('a'))
+        assert.is_true(base:any('x', 'd'))
+        assert.is_true(base:any({'x', 'd'}))
+        assert.is_true(base:any('b', 'x'))
+
+        assert.is_false(base:any('m'))
+        assert.is_false(base:any({'x', 'm'}))
+        assert.is_false(base:any('x', 'm'))
+    end)
+
+    it(':none should return true it does not have any passed items', function()
+        assert.is_true(base:none('x'))
+        assert.is_false(base:none('x', 'd'))
+        assert.is_false(base:none({'x', 'd'}))
+        assert.is_false(base:none('b', 'x'))
+
+        assert.is_false(base:none('a'))
+        assert.is_true(base:none({'x', 'm'}))
+        assert.is_true(base:none('x', 'm'))
+    end)
+
+    it(':add should be able to add unique items', function()
+        base:add('e')
         assert.same(5, #base)
-        assert.same('abc, def, ghi, jkl, mno', base:tostring())
-        base:add('mno')
-        assert.same('abc, def, ghi, jkl, mno', base:tostring())
-        base = base + 'pqr'
-        assert.same('abc, def, ghi, jkl, mno, pqr', base:tostring())
-        base:add{'stu', 'vwx'}
-        assert.same('abc, def, ghi, jkl, mno, pqr, stu, vwx', base:tostring())
+        assert.same({'a', 'b', 'c', 'd', 'e'}, base)
+        base:add('e')
+        assert.same({'a', 'b', 'c', 'd', 'e'}, base)
+        base = base + 'f'
+        assert.same({'a', 'b', 'c', 'd', 'e', 'f'}, base)
+        base:add{'g', 'h'}
+        assert.same({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}, base)
+        base:add('a', 'i')
+        assert.same({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'}, base)
     end)
 
-    it('should remove items', function()
-        base:remove{'vwx', 'stu'}
-        assert.same('abc, def, ghi, jkl, mno, pqr', base:tostring())
-        base:remove('pqr')
-        assert.same('abc, def, ghi, jkl, mno', base:tostring())
-        base = base - 'mno'
-        assert.same('abc, def, ghi, jkl', base:tostring())
-        base = base - {'jkl', 'ghi'}
-        assert.same('abc, def', base:tostring())
+    it(':remove should remove items', function()
+        base:remove{'a', 'c'}
+        assert.same({'b', 'd'}, base)
+        base:remove('c')
+        assert.same({'b', 'd'}, base)
+
+        base = String_Array{'a', 'b', 'c', 'd'}
+        base:remove('b', 'a', 'b', 'd')
+        assert.same({'c'}, base)
+        base:remove('b', 'a', 'b', 'd')
+        assert.same({'c'}, base)
+
+        base = String_Array{'a', 'b', 'c', 'd'}
+        base = base - 'b'
+        assert.same({'a', 'c', 'd'}, base)
+        base = base - {'a', 'd'}
+        assert.same({'c'}, base)
     end)
 
-    it('should return true if it has passed items', function()
-        assert.is_true(base:has('abc'))
-        assert.is_true(base:has({'abc', 'def'}))
-        assert.is_false(base:has('ghi'))
-        assert.is_false(base:has({'def', 'ghi'}))
-        assert.is_true(base('def'))
-        assert.is_false(base('ghi'))
-    end)
 
-    it('should toggle values', function()
-        base:toggle('def')
-        assert.same('abc', base:tostring())
-        base:toggle({'abc', 'def', 'ghi'})
-        assert.same('def, ghi', base:tostring())
-    end)
+
+    -- it('should toggle values', function()
+    --     base:toggle('def')
+    --     assert.same('abc', base:tostring())
+    --     base:toggle({'abc', 'def', 'ghi'})
+    --     assert.same('def, ghi', base:tostring())
+    -- end)
 
     it('should clear all values', function()
         base:clear()
-        assert.same('', base:tostring())
+        assert.same({}, base)
     end)
 
-    it('should concat', function()
-        base = base .. "abc"
-        assert.same('abc', base:tostring())
-        base = base .. {'def, ghi'}
-        assert.same('abc, def, ghi', base:tostring())
-        local rhs = String_Array{'jkl', 'mno'}
-        base = base .. rhs
-        assert.same('abc, def, ghi, jkl, mno', base:tostring())
-        local lhs = {'pqr', 'stu'}
-        base = lhs .. base
-        assert.same('abc, def, ghi, jkl, mno, pqr, stu', base:tostring())
+    it(':concat should concat', function()
+        -- LHS -- {'a', 'b', 'c', 'd'}
+        assert.same({'a', 'b', 'c', 'd', 'e'}, base .. 'e')
+        assert.same(same, base)
+        assert.same({'a', 'b', 'c', 'd', 'e'}, base .. {'e'})
+        assert.same(same, base)
+        assert.same({'a', 'b', 'c', 'd', 'e', 'f'}, base .. {'e', 'f'})
+        assert.same(same, base)
 
+        -- Both
+        local rhs = String_Array{'a', 'e', 'f'}
+        assert.same({'a', 'b', 'c', 'd', 'e', 'f'}, base .. rhs)
+        assert.same(same, base)
+        assert.same({'a', 'e', 'f'}, rhs)
+
+        -- RHS
+        assert.same({'e', 'a', 'b', 'c', 'd'}, 'e' .. base)
+        assert.same(same, base)
+        assert.same({'e', 'a', 'b', 'c', 'd'}, {'e'} .. base)
+        assert.same(same, base)
+        assert.same({'e', 'f', 'a', 'b', 'c', 'd'}, {'e', 'f'} .. base)
+        assert.same(same, base)
     end)
 end)
--- stdlib/utils/classes/string_array.lua
+++ stdlib/utils/classes/string_array.lua
@@ -1,5 +1,6 @@
 --- String Array Metatable
--- For working with string arrays without duplicate values
+-- For working with string arrays without duplicate values.
+-- string arrays are mutated in place.
 -- @classmod string_array
 
 local M = {
@@ -7,68 +8,113 @@ local M = {
 }
 local metatable
 
---- Does this array contain name.
--- @tparam string name The string to find.
--- @treturn boolean string is in array
-function M:has(name)
-    local type = type(name)
-    if type == 'table' then
-        for _, str in pairs(name) do
-            if not self:has(str) then
-                return false
+local allowed = {
+    ['string'] = true,
+    ['number'] = true
+}
+
+local function new(array, make_unique)
+    if type(array) == 'table' then
+        return setmetatable(array, metatable)
+    end
+end
+
+local function index_of(self, param)
+    for index, str in ipairs(self) do
+        if param == str then
+            return index
+        end
+    end
+end
+
+--- Does this array have all of the passed strings.
+-- @tparam string|{string,...} ...
+-- @treturn boolean every passed string is in the array
+function M:all(...)
+    local params = type(...) == 'table' and (...) or {...}
+    local count = 0
+    for _, param in ipairs(params) do
+        assert(allowed[type(param)], 'Paramater type not allowed.')
+        for _, str in ipairs(self) do
+            if param == str then
+                count = count + 1
+                if count == #params then
+                    return true
+                end
             end
         end
-        return true
     end
+    return false
+end
+M.has = M.all
 
-    assert(type == 'string', 'name must be a string')
-    for _, str in ipairs(self) do
-        if str == name then
-            return true
+--- Does this array have any of the passed strings.
+-- @tparam string|{string,...} ...
+-- @treturn boolean any passed string is in the array
+function M:any(...)
+    local params = type(...) == 'table' and (...) or {...}
+    for _, param in ipairs(params) do
+        assert(allowed[type(param)], 'Paramater type not allowed.')
+        for _, str in ipairs(self) do
+            if param == str then
+                return true
+            end
         end
     end
     return false
 end
 
+--- Does this array have none of the passed strings.
+-- @tparam string|{string,...} ...
+-- @treturn boolean no passed strings are in the array
+function M:none(...)
+    local params = type(...) == 'table' and (...) or {...}
+    for _, param in ipairs(params) do
+        assert(allowed[type(param)], 'Paramater type not allowed.')
+        for _, str in ipairs(self) do
+            if param == str then
+                return false
+            end
+        end
+    end
+    return true
+end
+
 --- Add a string to the array if it doesn't exist in the array.
 -- @tparam string name
--- @treturn self
-function M:add(name)
-    local type = type(name)
-    if type == 'table' then
-        for _, str in pairs(name) do
-            self:add(str)
+-- @treturn string_array
+function M:add(...)
+    local params = type(...) == 'table' and (...) or {...}
+    local insert = {}
+    for _, param in ipairs(params) do
+        assert(allowed[type(param)], 'Paramater type not allowed.')
+        local i = #insert + 1
+        insert[i] = param
+        for _, str in ipairs(self) do
+            if param == str then
+                insert[i] = nil
+                break
+            end
         end
-        return self
     end
-
-    assert(type == 'string', 'name must be a string')
-    for _, str in ipairs(self) do
-        if str == name then
-            return self
-        end
+    for _, add in ipairs(insert) do
+        rawset(self, #self + 1, add)
     end
-    table.insert(self, name)
     return self
 end
 
---- Remove the string from the array if it exists.
+--- Remove the strings from the array if they exist.
 -- @tparam string name
--- @treturn self
-function M:remove(name)
-    local type = type(name)
-    if type == 'table' then
-        for _, str in pairs(name) do
-            self:remove(str)
-        end
-        return self
-    end
-
-    assert(type == 'string', 'name must be a string')
-    for i, str in ipairs(self) do
-        if str == name then
-            table.remove(self, i)
-            return self
+-- @treturn string_array
+function M:remove(...)
+    local params = type(...) == 'table' and (...) or {...}
+    for _, param in pairs(params) do
+        assert(allowed[type(param)], 'Paramater type not allowed.')
+        for i = #self, 1, -1 do
+            if param == self[i] then
+                table.remove(self, i)
+                break
+            end
         end
     end
     return self
@@ -78,23 +124,35 @@ end
 -- @tparam string name
 -- @treturn self
 function M:toggle(name)
-    local type = type(name)
-    if type == 'table' then
-        for _, str in pairs(name) do
-            self:toggle(str)
-        end
-        return self
-    end
-
-    assert(type == 'string', 'name must be a string')
-    for i, str in ipairs(self) do
-        if str == name then
-            table.remove(self, i)
-            return self
+    local params = type(...) == 'table' and (...) or {...}
+    local insert = {}
+    for _, param in pairs(params) do
+        assert(allowed[type(param)], 'Paramater type not allowed.')
+        local index = index_of(self, param)
+        if index then
+            table.remove(self, index)
+        else
+            rawset(self, #self + 1, param)
         end
     end
-    table.insert(self, name)
     return self
+    -- local type = type(name)
+    -- if type == 'table' then
+    --     for _, str in pairs(name) do
+    --         self:toggle(str)
+    --     end
+    --     return self
+    -- end
+
+    -- assert(type == 'string', 'name must be a string')
+    -- for i, str in ipairs(self) do
+    --     if str == name then
+    --         table.remove(self, i)
+    --         return self
+    --     end
+    -- end
+    -- table.insert(self, name)
+    -- return self
 end
 
 --- Clear the array returning an empty array object
@@ -116,27 +174,49 @@ end
 -- @tparam string|string-array rhs
 -- @treturn string-array
 function M:concat(rhs)
-    if getmetatable(self) == metatable then
-        return self:add(rhs)
-    else
-        return rhs:add(self)
+    local ret = new {}
+    ret:add(self)
+    ret:add(rhs)
+    return ret
+end
+
+local dict_len = table_size or function(self)
+        local count = 0
+        for _ in pairs(self) do
+            count = count + 1
+        end
+        return count
     end
+
+function M:dictionary(func, ...)
+    local dict = {}
+    for index, value in pairs(self) do
+        dict[value] = func and func(value, index, ...) or index
+    end
+    return setmetatable(dict, {__len = dict_len})
+end
+
+function M:sort(comp)
+    table.sort(self, comp)
+    return self
+end
+
+--- Metamethods
+-- @section Metamethods
+
+local function __newindex(self, _, v)
+    self:add(v)
 end
 
 --- The following metamethods are provided.
 -- @table metatable
 metatable = {
     __index = M, -- Index to the string array class.
+    __newindex = __newindex,
     __tostring = M.tostring, -- tostring.
     __concat = M.concat, -- adds the right hand side to the object.
     __add = M.add, -- Adds a string to the string-array object.
-    __sub = M.remove, -- Removes a string from the string-array object.
-    __unm = M.clear, -- Clears the array.
-    __call = M.has -- Array contains this string.
+    __sub = M.remove -- Removes a string from the string-array object.
 }
 
-return function(array)
-    if type(array) == 'table' then
-        return setmetatable(array, metatable)
-    end
-end
+return new

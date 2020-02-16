7212e3065149512a5d18335c1746858a8fcd3661
Nexela <nexela@gmail.com>
2020-02-15 20:27:17 -0500

    Add Entity.get_minable_item Add Entity:is_player_placeable

--- stdlib/data/entity.lua
+++ stdlib/data/entity.lua
@@ -8,4 +8,20 @@ local Entity = {
 }
 setmetatable(Entity, Entity)
 
+function Entity:get_minable_item()
+    local Item = require('__stdlib__/stdlib/data/item')
+    if self:is_valid() then
+        local m = self.minable
+        return Item(m and (m.result or (m.results and (m.results[1] or m.results.name))), nil, self.options)
+    end
+    return Item()
+end
+
+function Entity:is_player_placeable()
+    if self:is_valid() then
+        return self:Flags():any('player-creation', 'placeable-player')
+    end
+    return false
+end
+
 return Entity
